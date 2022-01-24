6796ce773fc524e45f514ec066ecb8d264dd818a
raihan <ahmad.raihannudin.yahya@gmail.com>
2022-01-24 13:54:21 +0700

    compress image jumbotron

--- src/component/home/Jumbotron.js
+++ src/component/home/Jumbotron.js
@@ -2,7 +2,7 @@ import { Container } from "react-bootstrap";
 
 const Jumbotron = () => {
   return(
-    <Container fluid className="p-5" style = {{backgroundImage : `url(/jumbotron.png)`, minHeight : '512px'}}>
+    <Container fluid className="p-5" style = {{backgroundImage : `url(/jumbotron-small.png)`, minHeight : '512px'}}>
       <h1 className="text-center" style={{color : '#fff', marginTop : '170px', textShadow : '2px 2px #1F1F1F'}}>Connect on DumbSound</h1>
       <p className="text-center text-break mx-auto" style={{color : '#fff', textShadow : '1px 1px #1F1F1F', maxWidth : '680px', fontSize : '24px'}}>Discovery, Stream, and share a constantly expanding mix of music from emerging and major artists around the world</p>
     </Container>
