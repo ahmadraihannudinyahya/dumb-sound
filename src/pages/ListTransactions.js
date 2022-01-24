e696db918d8d575e0d1e2a8921394966ad500591
raihan <ahmad.raihannudin.yahya@gmail.com>
2022-01-24 20:14:24 +0700

    fix bug navbar

--- src/pages/ListTransactions.js
+++ src/pages/ListTransactions.js
@@ -26,7 +26,7 @@ const ListTransactions = () => {
     fetchListData();
   };
   return(
-    <Container fluid style = {{backgroundColor : "#161616", minHeight : '90vh'}} className = 'pt-5 px-sm-1 px-md-5'>
+    <Container fluid style = {{backgroundColor : "#161616", minHeight : '100vh', paddingTop : '120px'}} className = 'px-sm-1 px-md-5'>
       <h2 className="mb-5" style={{color : '#fff'}}>Incoming Transaction</h2>
       <TableTransaction>
         {listData.map((data, index) => <BodyTableTransaction key = {index} {...data} index = {index} handleAproveTransaction={handleAproveTransaction} handleCancelTransaction={handleCancelTransaction} setModal={setImageModal}/>)}
